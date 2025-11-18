ae7cab6a87572f57cde6bdfd91639025f8b499d2
EvmSerhii <serhii.ihnatiuk@protofire.io>
2025-11-18 15:12:16 +0100

    add .env_example

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -15,7 +21,18 @@ const config: HardhatUserConfig = {
     artifacts: "artifacts"
   },
   networks: {
-    hardhat: {}
+    hardhat: {},
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
+    },
+    mainnet: {
+      url: MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   }
 };
 
